bf92b01d8a9f5ce567cc7dba7abfa6019b48a7fe
tal0311 <tal.amit0311@gmail.com>
2022-08-10 23:38:26 +0300

    basic CRUD

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ var items = [
     content: 'some task',
   },
   {
-    _id: '101',
+    _id: '102',
     content: 'some task',
   },
   {
-    _id: '101',
+    _id: '103',
     content: 'some task',
   },
   {
-    _id: '101',
+    _id: '104',
     content: 'some task',
   },
 ]
@@ -30,10 +30,44 @@ class CRUD {
   }
   remove(entityId) {
     const idx = this.data.findIndex((item) => item._id === entityId)
-    this.data = this.data.splice(idx, 1)
-    this.setData(this.data)
+    this.data.splice(idx, 1)
+  }
+  update(entity) {
+    const idx = this.data.findIndex((item) => item._id === entity._id)
+    this.data.splice(idx, 1, entity)
   }
   setData(data) {
     this.data = data
   }
+  getById(entityId) {
+    return this.data.find((item) => item._id === entityId)
+  }
+}
+
+const crudService = new CRUD(items)
+console.log('crudService:', crudService)
+
+// console.log(remove('101'))
+function remove(taskId) {
+  crudService.remove(taskId)
+}
+
+const task = {
+  _id: '102',
+  content: 'updated',
+}
+
+update(task)
+function update(task) {
+  crudService.update(task)
+}
+
+console.log('getting all items:', query())
+function query() {
+  return crudService.query()
+}
+
+console.log('getting item:', getById('101'))
+function getById(taskId) {
+  return crudService.getById(taskId)
 }
