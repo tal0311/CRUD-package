db5f6d8f9256be6288ccfba983c7b1335a07dd9b
tal0311 <tal.amit0311@gmail.com>
2022-08-10 23:11:34 +0300

    query/remove

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 console.log('package')
+
+var items = [
+  {
+    _id: '101',
+    content: 'some task',
+  },
+  {
+    _id: '101',
+    content: 'some task',
+  },
+  {
+    _id: '101',
+    content: 'some task',
+  },
+  {
+    _id: '101',
+    content: 'some task',
+  },
+]
+
+class CRUD {
+  constructor(data) {
+    this.data = data
+  }
+
+  query() {
+    // filtering options
+    return this.data
+  }
+  remove(entityId) {
+    const idx = this.data.findIndex((item) => item._id === entityId)
+    this.data = this.data.splice(idx, 1)
+    this.setData(this.data)
+  }
+  setData(data) {
+    this.data = data
+  }
+}
