690ff08e665af63045b72f280a40802cb0b06161
tal0311 <tal.amit0311@gmail.com>
2022-08-11 08:19:11 +0300

    getEmptyItem

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ var items = [
   {
     _id: '101',
     content: 'some task',
+    isDone: true,
+    createdAt: Date.now(),
+    reviews: [
+      {
+        _id: '101',
+        reviewContent: 'some content',
+      },
+    ],
   },
   {
     _id: '102',
@@ -20,8 +28,12 @@ var items = [
 ]
 
 class CRUD {
-  constructor(data) {
+  constructor(data, localStorage, key, promiseBased) {
     this.data = data
+    this.isSaveToLS = localStorage
+    this.lcKey = key
+    this.isAsync = promiseBased
+    // this.itemId = CRUD.makeId()
   }
 
   query() {
@@ -42,24 +54,71 @@ class CRUD {
   getById(entityId) {
     return this.data.find((item) => item._id === entityId)
   }
+
+  add(entity) {
+    entity._id = this.#makeId()
+    this.data = [...this.data, entity]
+  }
+
+  getEmptyItem() {
+    const item = JSON.parse(JSON.stringify(this.data[0]))
+    for (let key in item) {
+      if (typeof item[key] === 'string') {
+        item[key] = ''
+      } else if (typeof item[key] === 'boolean') {
+        item[key] = false
+      } else if (typeof item[key] === 'object') {
+        item[key] = null
+      } else {
+        item[key] = 0
+      }
+    }
+    return item
+  }
+  // private methods
+  #makeId(length = 5) {
+    var txt = ''
+    var possible =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+
+    for (var i = 0; i < length; i++) {
+      txt += possible.charAt(Math.floor(Math.random() * possible.length))
+    }
+    console.log(txt)
+    return txt
+  }
 }
 
-const crudService = new CRUD(items)
+const crudService = new CRUD(items, true, 'tasks', true)
 console.log('crudService:', crudService)
 
 // console.log(remove('101'))
-function remove(taskId) {
-  crudService.remove(taskId)
-}
+// function remove(taskId) {
+//   crudService.remove(taskId)
+// }
+
+// const task = {
+//   _id: '102',
+//   content: 'updated',
+// }
+
+// update(task)
+// function update(task) {
+//   crudService.update(task)
+// }
 
 const task = {
-  _id: '102',
-  content: 'updated',
+  content: 'just crated',
+}
+const task1 = {
+  content: 'just crated',
 }
 
-update(task)
-function update(task) {
-  crudService.update(task)
+add(task)
+add(task1)
+
+function add(task) {
+  crudService.add(task)
 }
 
 console.log('getting all items:', query())
@@ -67,7 +126,12 @@ function query() {
   return crudService.query()
 }
 
-console.log('getting item:', getById('101'))
-function getById(taskId) {
-  return crudService.getById(taskId)
+// console.log('getting item:', getById('101'))
+// function getById(taskId) {
+//   return crudService.getById(taskId)
+// }
+
+getEmptyTask()
+function getEmptyTask() {
+  console.log(crudService.getEmptyItem())
 }
